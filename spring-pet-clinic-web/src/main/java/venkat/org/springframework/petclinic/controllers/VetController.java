5c317443763070353f2244f554af81be29029750
Utla, Venkatasubbaiah (ES) <Venkatasubbaiah.Utla@ADP.com>
2018-12-18 21:42:35 +0530

    Implemented Vet List. closes #17

--- spring-pet-clinic-web/src/main/java/venkat/org/springframework/petclinic/controllers/VetController.java
+++ spring-pet-clinic-web/src/main/java/venkat/org/springframework/petclinic/controllers/VetController.java
@@ -1,14 +1,20 @@
 package venkat.org.springframework.petclinic.controllers;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import venkat.org.springframework.petclinic.services.VetService;
 
 @Controller
+@AllArgsConstructor
 public class VetController {
+    private final VetService vetService;
 
     @RequestMapping(value = {"/vets", "/vets/index", "/vets/index.html"}, method = {RequestMethod.GET})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
--- spring-pet-clinic-web/src/test/java/venkat/org/springframework/petclinic/controllers/VetControllerTest.java
+++ spring-pet-clinic-web/src/test/java/venkat/org/springframework/petclinic/controllers/VetControllerTest.java
@@ -1,14 +1,33 @@
 package venkat.org.springframework.petclinic.controllers;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
+import venkat.org.springframework.petclinic.services.VetService;
+import venkat.org.springframework.petclinic.services.map.VetServiceMap;
 
 
 public class VetControllerTest {
 
+    private VetService vetService;
+
+    @Before
+    public void setUp() {
+        vetService = new VetServiceMap();
+    }
+
+    @After
+    public void tearDown() {
+        vetService = null;
+    }
+
     @Test
     public void listVets() {
-        VetController vetController = new VetController();
-        Assert.assertEquals("Vet List view name is not matching", "vets/index", vetController.listVets());
+        Model model = new ConcurrentModel();
+        VetController vetController = new VetController(vetService);
+        Assert.assertEquals("Vet List view name is not matching", "vets/index", vetController.listVets(model));
     }
 }
