fbdc074fd676c6393af28fe86f6bf74ffd96c654
Utla, Venkatasubbaiah (ES) <Venkatasubbaiah.Utla@ADP.com>
2018-12-18 21:27:14 +0530

    Implemented Owner List page. closes #18

--- spring-pet-clinic-web/src/main/java/venkat/org/springframework/petclinic/controllers/OwnerController.java
+++ spring-pet-clinic-web/src/main/java/venkat/org/springframework/petclinic/controllers/OwnerController.java
@@ -1,14 +1,25 @@
 package venkat.org.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import venkat.org.springframework.petclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
 
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping(value = {"/owners", "/owners/index", "/owners/index.html"}, method = {RequestMethod.GET})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
+
+
 }
--- spring-pet-clinic-web/src/test/java/venkat/org/springframework/petclinic/controllers/OwnerControllerTest.java
+++ spring-pet-clinic-web/src/test/java/venkat/org/springframework/petclinic/controllers/OwnerControllerTest.java
@@ -1,13 +1,32 @@
 package venkat.org.springframework.petclinic.controllers;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
+import venkat.org.springframework.petclinic.services.OwnerService;
+import venkat.org.springframework.petclinic.services.map.OwnerServiceMap;
 
 public class OwnerControllerTest {
+    private OwnerService ownerService;
+
+    @Before
+    public void setup() {
+        ownerService = new OwnerServiceMap();
+    }
+
+    @After
+    public void tearDown() {
+        ownerService = null;
+    }
 
     @Test
     public void listOwners() {
-        OwnerController ownerController = new OwnerController();
-        Assert.assertEquals("View name not matching", "owners/index", ownerController.listOwners());
+
+        OwnerController ownerController = new OwnerController(ownerService);
+        Model model = new ConcurrentModel();
+        Assert.assertEquals("View name not matching", "owners/index", ownerController.listOwners(model));
     }
 }
